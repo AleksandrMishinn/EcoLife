6ff0f5ed860bee10ea35e6b5033f7f97b05aae01
alexandrmishinn <alexandrmishinn@gmail.com>
2020-03-10 09:18:52 +0300

    Database connection + application layers

---
+++ src/main/java/com/ecolife/ecolife/EcoLifeApplication.java
@@ -0,0 +1,13 @@
+package com.ecolife.ecolife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoLifeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcoLifeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecolife/ecolife/EcoLifeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecolife.ecolife;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoLifeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
